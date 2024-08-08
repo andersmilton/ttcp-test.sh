c92ff7fcbced82ae3e8bb9b486e28439d85d1054
kludg <anders.milton@gmail.com>
2024-08-08 18:07:30 +0200

    Added delays, removed progress indicator, fixes

--- ttcp-test.sh
+++ ttcp-test.sh
@@ -3,7 +3,8 @@
 # ttcp-test
 # (c) Anders Milton, Patrik Axelsson 2024
 #
-# Performs network speed tests to/from an Amiga on the network using ttcp.
+# Performs network speed tests to/from an Amiga on the network using ttcp
+# and rsh.
 #
 # Version history:
 # v0.1 Initial release
@@ -11,14 +12,36 @@
 #      receiving end.
 # v0.3 Waiting for the forked ttcp server to finish before getting the
 #      transfer speed
+# v0.4 Added delays for older Amigas to be able to catch up. Tested on stock
+#      68000 at 7 MHz.
+#      Removed the progress indicator from the transmission part as we didn't
+#      have one for receiving.
+#      Added a variable for the path of the local rsh binary.
 #
 
-echo -e "ttcp-test v0.3" >&2
+echo -e "ttcp-test v0.4" >&2
 echo -e "(c) Anders Milton, Patrik Axelsson 2024\n" >&2
 [ $# -ne 1 ] && { echo "Usage: $0 <HOST>" >&2; exit 1; }
 HOST=${1}
 
+echo -n "Existance of ttcp on the local (this) machine... " >&2
 LTTCP_BIN=`which ttcp`
+if [ $? -eq 0 ]; then
+	echo ${LTTCP_BIN} >&2
+else
+	echo "Could not determine the path for the local ttcp binary" >&2
+	exit 1
+fi
+
+echo -n "Existance of rsh on the local machine... " >&2
+LRSH_BIN=`which rsh`
+if [ $? -eq 0 ]; then
+	echo ${LRSH_BIN} >&2
+else
+        echo "Could not determine the path for the local rsh binary" >&2
+        exit 1
+fi
+
 LOCAL_IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 RTTCP_BIN="C:ttcp"
 RTTCP_CHK="if exists ${RTTCP_BIN}; echo 1; else echo 2; endif"
@@ -36,15 +59,15 @@ else
 fi
 
 echo -n "Is remote host (${HOST}) reachable... " >&2;
-if [ `rsh ${HOST} echo 1` -eq 1 ]; then
+if [ `${LRSH_BIN} ${HOST} echo 1` -eq 1 ]; then
     echo "yes" >&2
 else
-    echo "Could not execute remote command. Is rsh installed on the remote system?" >&2
+    echo "Could not execute remote command. Is rsh installed and activated on the remote system?" >&2
     exit 1
 fi
 
 echo -n "Is ttcp installed on remote host... " >&2
-if [ `rsh ${HOST} ${RTTCP_CHK}` -eq 1 ]; then
+if [ `${LRSH_BIN} ${HOST} ${RTTCP_CHK}` -eq 1 ]; then
     echo "yes" >&2
 else
     echo "${RTTCP_BIN} could not be found on remote system." >&2
@@ -55,18 +78,17 @@ fi
 TSPEEDS=""
 echo -e "\nTransmitting:" >&2
 for i in {1..5}; do
-    echo -n "#$i Starting remote ttcp server... " >&2
-    rsh ${HOST} ${RTTCPR_CMD} > /dev/null &
+    echo -n "$i/5 Starting remote ttcp server... " >&2
+    ${LRSH_BIN} ${HOST} ${RTTCPR_CMD} > /dev/null &
     PID=$!
-    sleep 0.5
+    sleep 3
     echo -n "Transmitting... " >&2
     ${LTTCPT_CMD} &>/dev/null
     while ps $PID | grep -o $PID >/dev/null; do
-        echo -ne "\b. " >&2
-        sleep 0.1
+	sleep 1
     done
-    KBPS=`rsh ${HOST} type ram:ttcp-test.temp | grep -Eo "[0-9]+\.[0-9][0-9] KB/sec" | cut -d' ' -f1`
-    rsh ${HOST} delete ram:ttcp-test.temp &>/dev/null
+    KBPS=`${LRSH_BIN} ${HOST} type ram:ttcp-test.temp | grep -Eo "[0-9]+\.[0-9][0-9] KB/sec" | cut -d' ' -f1`
+    ${LRSH_BIN} ${HOST} delete ram:ttcp-test.temp &>/dev/null
     echo "done ($KBPS KB/s)" >&2
     TSPEEDS="$TSPEEDS $KBPS"
 done
@@ -77,17 +99,18 @@ echo -e "\nReceiving:" >&2
 
 echo -n "Building remote script... " >&2
 for i in {1..5}; do
-    rsh ${HOST} "echo \"wait 1\" >>ram:ttcp-test.temp"
-    rsh ${HOST} "echo \"${RTTCPT_CMD}\" >>ram:ttcp-test.temp"
+    ${LRSH_BIN} ${HOST} "echo \"wait 3\" >>ram:ttcp-test.temp"
+    ${LRSH_BIN} ${HOST} "echo \"${RTTCPT_CMD}\" >>ram:ttcp-test.temp"
 done
-rsh ${HOST} "protect ram:ttcp-test.temp +se"
+${LRSH_BIN} ${HOST} "protect ram:ttcp-test.temp +se"
 echo "done" >&2
 
 echo -n "Starting remote ttcp client script... " >&2
-rsh ${HOST} ram:ttcp-test.temp &
+${LRSH_BIN} ${HOST} ram:ttcp-test.temp &
+sleep 1
 echo "done" >&2
 for i in {1..5}; do
-    echo -n "#$i Starting local ttcp server... " >&2
+    echo -n "$i/5 Starting local ttcp server... " >&2
     echo -n "Receiving... " >&2
     KBPS=`${LTTCPR_CMD} 2>&1 |
     while read -r line; do
@@ -97,7 +120,7 @@ for i in {1..5}; do
     RSPEEDS="$RSPEEDS $KBPS"
 done
 echo -n "Deleting remote client script... " >&2
-rsh ${HOST} delete ram:ttcp-test.temp &>/dev/null && echo "done" >&2 || echo "Could not delete script" >&2
+${LRSH_BIN} ${HOST} delete ram:ttcp-test.temp &>/dev/null && echo "done" >&2 || echo "Could not delete script" >&2
 
 echo "" >&2
 TSPEEDS=`echo $TSPEEDS | tr ' ' ','`
@@ -107,3 +130,4 @@ RSPEEDS_SORTED=`echo $RSPEEDS | tr ',' '\n' | sort | tr '\n' ','`
 echo -e " ,PASS 1,PASS 2,PASS 3,PASS 4,PASS 5,|,MIN,MAX,MEDIAN\n\
 TRANSMIT,`echo $TSPEEDS`,|,`echo $TSPEEDS_SORTED | cut -d',' -f1`,`echo $TSPEEDS_SORTED | cut -d',' -f5`,`echo $TSPEEDS_SORTED | cut -d',' -f3`\n\
 RECEIVE,`echo $RSPEEDS`,|,`echo $RSPEEDS_SORTED | cut -d',' -f1`,`echo $RSPEEDS_SORTED | cut -d',' -f5`,`echo $RSPEEDS_SORTED | cut -d',' -f3`" | column -t -s','
+exit 0
